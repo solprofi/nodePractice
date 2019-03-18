f6cd9d22c9e7c1283ac651c34fb9d1da7273fba9
Solohor <roman.solohor@sap.com>
2019-03-18 13:16:37 +0100

    added worker functions to check for domain status

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-//container for all environments
+// container for all environments
 const environments = {};
 
-//create staging (default) env
+// create staging (default) env
 environments.staging = {
   httpPort: 3005,
   httpsPort: 3006,
@@ -11,11 +11,11 @@ environments.staging = {
   twilio: {
     accountSid: 'ACf3c2cd3bfaaa4ae25633d26dad52742e',
     authToken: '491eaae5a5a882824b6b260a14370058',
-    sender: '+12013471874'
-  }
+    sender: '+12013471874',
+  },
 };
 
-//create production (default) env
+// create production (default) env
 environments.production = {
   httpPort: 6000,
   httpsPort: 6001,
@@ -25,14 +25,14 @@ environments.production = {
   twilio: {
     accountSid: 'ACf3c2cd3bfaaa4ae25633d26dad52742e',
     authToken: '491eaae5a5a882824b6b260a14370058',
-    sender: '+12013471874'
-  }
+    sender: '+12013471874',
+  },
 };
 
-//determine which env was passed into command line
+// determine which env was passed into command line
 const env = process.env.NODE_ENV;
 const currentEnv = typeof env === 'string' ? env.toLowerCase() : '';
 
-//check if environments object has the current env
+// check if environments object has the current env
 const envToExport = typeof environments[currentEnv] === 'object' ? environments[currentEnv] : environments.staging;
 module.exports = envToExport;
