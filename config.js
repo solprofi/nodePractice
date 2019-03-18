73264143d1e244a8766152d07bd1f320f9d0c4bb
Solohor <roman.solohor@sap.com>
2019-03-18 09:39:18 +0100

    refactored index file

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const environments = {};
 
 //create staging (default) env
 environments.staging = {
-  httpPort: 3000,
-  httpsPort: 3001,
+  httpPort: 3005,
+  httpsPort: 3006,
   envName: 'staging',
   hashingSecret: 'This is a hashing secret',
   maxChecks: 5,
   twilio: {
     accountSid: 'ACf3c2cd3bfaaa4ae25633d26dad52742e',
     authToken: '491eaae5a5a882824b6b260a14370058',
-    sender: '+12013471874',
+    sender: '+12013471874'
   }
 };
 
@@ -25,14 +25,14 @@ environments.production = {
   twilio: {
     accountSid: 'ACf3c2cd3bfaaa4ae25633d26dad52742e',
     authToken: '491eaae5a5a882824b6b260a14370058',
-    sender: '+12013471874',
+    sender: '+12013471874'
   }
 };
 
 //determine which env was passed into command line
 const env = process.env.NODE_ENV;
-const currentEnv = typeof (env) === 'string' ? env.toLowerCase() : '';
+const currentEnv = typeof env === 'string' ? env.toLowerCase() : '';
 
 //check if environments object has the current env
-const envToExport = typeof (environments[currentEnv]) === 'object' ? environments[currentEnv] : environments.staging;
+const envToExport = typeof environments[currentEnv] === 'object' ? environments[currentEnv] : environments.staging;
 module.exports = envToExport;
--- index.js
+++ index.js
@@ -1,104 +1,16 @@
 //dependencies
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const fs = require('fs');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const server = require('./lib/server');
+// const workers = require('./lib/workers');
 
-const unifiedServer = (req, res) => {
-  //parse the url
-  const parsedUrl = url.parse(req.url, true);
+let app = {};
 
-  //get the path
-  const path = parsedUrl.pathname;
+app.init = () => {
+  //start the server
+  server.init();
+  //start the workers
+  // workers.init();
+};
 
-  //trim the path
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+app.init();
 
-  //get the http method
-  const method = req.method.toLowerCase();
-
-  //get the query
-  const queryObject = parsedUrl.query;
-
-  //get the headers
-  const headers = req.headers;
-
-  //get the payload
-  const decoder = new StringDecoder('utf-8');
-  let buffer = '';
-
-  req.on('data', data => {
-    buffer += decoder.write(data);
-  });
-
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    //choose the handler
-    const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-    //construct the data object
-    const data = {
-      payload: helpers.parseJsonToObj(buffer),
-      method,
-      trimmedPath,
-      queryObject,
-      headers,
-    };
-
-    //call the router
-    chosenHandler(data, (statusCode, payload) => {
-      //set the status code
-      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
-
-      //set the payload
-      payload = typeof (payload) == 'object' ? payload : {};
-
-      //convert the payload to a string
-      const payloadString = JSON.stringify(payload);
-
-      //send the response
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      // logging
-      console.log(`responding with status code ${statusCode} and payload`, payloadString);
-    });
-  });
-}
-
-//create servers
-const httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
-
-const httpsServerOptions = {
-  'key': fs.readFileSync('./https/key.pem'),
-  'cert': fs.readFileSync('./https/cert.pem'),
-}
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-  unifiedServer(req, res);
-});
-
-
-//start the servers and listen on the ports
-httpServer.listen(config.httpPort, () => {
-  console.log(`the http server is listening on port ${config.httpPort} in ${config.envName} mode`);
-});
-
-httpsServer.listen(config.httpsPort, () => {
-  console.log(`the https server is listening on port ${config.httpsPort} in ${config.envName} mode`);
-});
-
-//define the router
-const router = {
-  'ping': handlers.ping,
-  'users': handlers.users,
-  'tokens': handlers.tokens,
-  'checks': handlers.checks,
-}
+module.exports = app;
